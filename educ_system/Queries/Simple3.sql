0cd429ae04e91772aad53eb0e367aa62a0aeba79
pashok nout <redkopavli@gmail.com>
2021-05-28 11:19:42 +0300

    Fix

--- educ_system/Queries/Simple3.sql
+++ educ_system/Queries/Simple3.sql
@@ -2,4 +2,4 @@ SELECT DISTINCT Courses.Name, Courses.Price
 FROM Courses 
 INNER JOIN Teachers ON Teachers.Id = Courses.TeacherId 
 INNER JOIN Subjects ON Subjects.Id = Teachers.SubjectId
-WHERE Subjects.Name = Õ;
+WHERE Subjects.Name = X;
--- educ_system/Queries/Simple5.sql
+++ educ_system/Queries/Simple5.sql
@@ -1,4 +1,4 @@
 SELECT DISTINCT Teachers.Name
 FROM Teachers
 INNER JOIN Courses ON Teachers.Id = Courses.TeacherId
-WHERE Courses.Price != Õ;
+WHERE Courses.Price != X;
